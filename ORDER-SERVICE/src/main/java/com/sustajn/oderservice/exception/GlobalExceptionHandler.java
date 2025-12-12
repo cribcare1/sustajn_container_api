91b2b1c6f5eedd4dfff3da330b90ad410eadd5b4
arunbapi1 <arun.panigrahi1@gmail.com>
2025-12-12 20:24:25 +0530

    pom issue fixed

--- ORDER-SERVICE/src/main/java/com/sustajn/oderservice/exception/GlobalExceptionHandler.java
+++ ORDER-SERVICE/src/main/java/com/sustajn/oderservice/exception/GlobalExceptionHandler.java
@@ -9,14 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import lombok.extern.slf4j.Slf4j;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
