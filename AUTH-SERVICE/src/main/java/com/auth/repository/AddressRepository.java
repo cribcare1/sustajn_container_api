19e31bad2e0068521e796c86f7885a45554343d8
ankit6370232757 <ankitkumarrath5@gmail.com>
2026-01-15 23:00:37 +0530

    address and subscriptionplan added

--- AUTH-SERVICE/src/main/java/com/auth/repository/AddressRepository.java
+++ AUTH-SERVICE/src/main/java/com/auth/repository/AddressRepository.java
@@ -3,7 +3,10 @@ package com.auth.repository;
 import com.auth.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    List<Address> findByUserIdAndStatus(Long userId, String status);
 }
