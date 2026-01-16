ea4d00339768318e779d46ae710520abe5b899e7
surajksnalabs <surajkn1405@gmail.com>
2026-01-16 12:08:09 +0530

    removed user defined constructor

--- ORDER-SERVICE/src/main/java/com/sustajn/oderservice/dto/LeasedReturnedResponse.java
+++ ORDER-SERVICE/src/main/java/com/sustajn/oderservice/dto/LeasedReturnedResponse.java
@@ -1,5 +1,6 @@
 package com.sustajn.oderservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,15 +8,16 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class LeasedReturnedResponse {
 
     private String monthYear;   // November-2025
     private String date;        // 22.11.2025
     private Long count;         // leased count
 
-    public LeasedReturnedResponse(String monthYear, String date, Long count) {
-        this.monthYear = monthYear;
-        this.date = date;
-        this.count = count;
-    }
+//    public LeasedReturnedResponse(String monthYear, String date, Long count) {
+//        this.monthYear = monthYear;
+//        this.date = date;
+//        this.count = count;
+//    }
 }
